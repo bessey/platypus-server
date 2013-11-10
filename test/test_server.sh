a4b18ac10bee6f228270dd987afd9df199abb23f
Chris Atkin <me@chrisatk.in>
2013-11-09 23:02:58 -0800

    Test server uses coffee in the repo, not globally installed coffeescript

--- test/test_server.sh
+++ test/test_server.sh
@@ -1,1 +1,1 @@
-FIREBASE_ENDPOINT="platypus-launchhack-test.firebaseIO.com" coffee ../platypus.coffee
+FIREBASE_ENDPOINT="platypus-launchhack-test.firebaseIO.com" ../node_modules/coffee-script/bin/coffee ../platypus.coffee
